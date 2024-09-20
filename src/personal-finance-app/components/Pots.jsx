10a7a0d165e9c12deb0200821337ffa313cc14ba
JesusVA760 <tonio4340406@gmail.com>
2024-09-20 08:27:47 -0700

    Pots

--- src/personal-finance-app/components/Pots.jsx
+++ src/personal-finance-app/components/Pots.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import arrow from "../starter-code/assets/images/icon-caret-right.svg";
 import savedSign from "../starter-code/assets/images/icon-nav-pots.svg";
 const Pots = () => {
+  const [saved, setSaved] = useState(0);
+  const [savings, setSavings] = useState(0);
+
   return (
     <div className="pots-container rounded-xl p-7 max-w-2xl bg-white mt-20">
       <div className="pots-heading flex flex-row">
         <div className="pots-title mr-auto mb-10">
-          <h3>Pots</h3>
+          <h3 className="text-2xl font-bold">Pots</h3>
         </div>
         <div className="pots-details">
           <button className="detail-button flex items-center gap-2 cursor-pointer">
@@ -16,13 +19,33 @@ const Pots = () => {
         </div>
       </div>
       <div className="pots-content">
-        <div className="pots-saved-total">
+        <div className="pots-saved-total flex align-center gap-8 bg-gray-500 w-fit px-12 py-5 rounded-md">
           <div className="pots-saved-total-img">
-            <img className="pots-image w-10" src={savedSign} alt="" />
+            <img className="pots-image w-11" src={savedSign} alt="" />
           </div>
-          <div className="pots-saved-total-value"></div>
+          <div className="pots-saved-total-value text-white">
+            <h4>Total Saved</h4>
+            <h2 className="saved-amount text-2xl">${saved}</h2>
+          </div>
         </div>
-        <div className="pots-more"></div>
+        <div className="pots-more">
+          <div className="pots-savings">
+            <h5>Savings</h5>
+            <h2>${savings}</h2>
+          </div>
+          <div className="pots-gift">
+            <h5>Gift</h5>
+            <h2>${savings}</h2>
+          </div>
+          <div className="pots-concert">
+            <h5>Concert Ticket</h5>
+            <h2>${savings}</h2>
+          </div>
+          <div className="pots-purchase">
+            <h5>New Laptop</h5>
+            <h2>${savings}</h2>
+          </div>
+        </div>
       </div>
     </div>
   );
