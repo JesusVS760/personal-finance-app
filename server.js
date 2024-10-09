041590921ded85db38676df75513f9b418f5bf07
JesusVA760 <tonio4340406@gmail.com>
2024-10-08 23:57:32 -0700

    Server Attempt

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-const express = require("express");
-const fs = require("fs");
+import express from "express";
+import fs from "fs";
+import cors from "cors";
+
 const app = express();
 const port = 5000;
+
+/// Cross- Origin Resource
+app.use(cors());
 
 app.use(express.json());
 
@@ -13,9 +18,7 @@ app.post("/data.json", (req, res) => {
       return res.status(500).json({ error: "Error reading the file" });
     }
     // Parse the existing JSON data
-
     const pots = JSON.parse(data || "[]");
-
     //Add the new pot
     pots.push(newPot);
 
